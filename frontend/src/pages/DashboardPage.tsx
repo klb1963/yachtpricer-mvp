0e3e28a08d722a6b7801689dc028d1512bd1d90e
Leonid Kleimann <k_l_b_1963@hotmail.com>
2025-07-08 16:57:46 +0200

    test(ci): verify deploy pipeline

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -2,8 +2,9 @@
 export default function DashboardPage() {
     return (
       <main className="centered">
-        <h1>Welcome to YachtPricer!</h1>
+        <h1>Welcome to YachtPricer! 🚤 </h1>
         <p>Your dashboard is ready.</p>
+        <p>This is new text.</p>
       </main>
     )
   }
